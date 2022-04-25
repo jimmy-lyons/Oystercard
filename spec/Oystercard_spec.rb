a92b6286129f5496374d01b98827816e88a33a6b
Erlantz <erlantzram@gmail.com>
2022-04-25 15:44:50 +0100

    top_up method and test added

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -4,4 +4,13 @@ describe Oystercard do
   it 'New instances of Oystercard have a balance of zero' do
     expect(subject.balance).to eq(0) 
   end
+
+  describe '#top_up' do
+
+    it 'adds topup value to balance' do
+      expect{ subject.top_up 100 }.to change{ subject.balance }.by 100
+    end
+
+  end
+
 end
